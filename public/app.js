3f26fcb5312e916d5075da0399e128a29da4b34a
Ryan Weaver <ryan@thatsquality.com>
2020-08-24 12:20:51 -0400

    Step 39: Stimulus: bootstrap code

--- public/app.js
+++ public/app.js
@@ -7,6 +7,14 @@ import ReactDOM from 'react-dom';
 // Could this somehow load a CSS file for us?
 import './styles.css';
 
+/* Stimulus Setup */
+import { Application } from 'stimulus'
+import { definitionsFromContext } from 'stimulus/webpack-helpers'
+const application = Application.start()
+const context = require.context('./controllers', true, /\.js$/)
+application.load(definitionsFromContext(context))
+/* End Stimulus Setup */
+
 const app = document.querySelector('#app');
 
 async function initializeStore() {
