76abceaaa54ed72fef43fc4365aa58eaeebc7128
Ryan Weaver <ryan@thatsquality.com>
2020-08-24 12:20:52 -0400

    Step 43: AJAX in Stimulus

--- public/controllers/store_controller.js
+++ public/controllers/store_controller.js
@@ -1,11 +1,15 @@
 import { Controller } from 'stimulus';
+import axios from 'axios';
 
 export default class extends Controller {
     static get targets() {
         return ['productList'];
     }
 
-    printProducts() {
-        this.productListTarget.innerHTML = 'Testing!'
+    async printProducts() {
+        const response = await axios.get('/products');
+        const products = response.data;
+
+        this.productListTarget.innerHTML = products.join(', ');
     }
 }
