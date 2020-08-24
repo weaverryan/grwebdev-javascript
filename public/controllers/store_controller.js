4de350b36fc6d9a8173032de89b920fe6af738f1
Ryan Weaver <ryan@thatsquality.com>
2020-08-24 12:20:52 -0400

    Step 42: Stimulus: hook up app

--- public/controllers/store_controller.js
+++ public/controllers/store_controller.js
@@ -1,5 +1,11 @@
 import { Controller } from 'stimulus';
 
 export default class extends Controller {
-    
+    static get targets() {
+        return ['productList'];
+    }
+
+    printProducts() {
+        this.productListTarget.innerHTML = 'Testing!'
+    }
 }
